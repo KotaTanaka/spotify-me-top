37ca65d9f55f1aba4952be8a630062e62c368c84
KotaTanaka <te11k.tanaka@gmail.com>
2022-03-07 00:45:42 +0900

    [style] add home links

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,4 +1,10 @@
-import Home from '~/containers/Home';
+import type { LinksFunction } from 'remix';
+
+import Home, { links as homeLinks } from '~/containers/Home/Home';
+
+export const links: LinksFunction = () => {
+  return [...homeLinks()];
+};
 
 export default function Index() {
   return <Home />;
