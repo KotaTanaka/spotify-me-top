83e214152d558f07a44b4fdd3a781db25a812dd1
KotaTanaka <te11k.tanaka@gmail.com>
2022-03-08 20:34:56 +0900

    [feat] setup spotify API axios

--- app/lib/axios.ts
+++ app/lib/axios.ts
@@ -1,9 +1,13 @@
 import axios, { AxiosError } from 'axios';
 
-import { SPOTIFY_ACCOUNTS_URL } from '~/constants';
+import { SPOTIFY_ACCOUNTS_URL, SPOTIFY_API_URL } from '~/constants';
 
 export const spotifyAccountsAxios = axios.create({
   baseURL: SPOTIFY_ACCOUNTS_URL,
+});
+
+export const spotifyApiAxios = axios.create({
+  baseURL: SPOTIFY_API_URL,
 });
 
 export const isAxiosError = (error: any): error is AxiosError => {
