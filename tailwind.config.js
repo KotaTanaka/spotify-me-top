d4fef43604d5dd18d8eb16ed169892d58381f114
KotaTanaka <te11k.tanaka@gmail.com>
2022-03-09 02:08:37 +0900

    [config] custom theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,21 @@ module.exports = {
   },
   plugins: [require('daisyui')],
   daisyui: {
-    prefix: 'daisy',
+    prefix: 'daisy-',
+    themes: [
+      {
+        mytheme: {
+          primary: '#a3e635',
+          secondary: '#58C7F3',
+          accent: '#F3CC30',
+          neutral: '#20134E',
+          'base-100': '#701a75',
+          info: '#53C0F3',
+          success: '#71EAD2',
+          warning: '#F3CC30',
+          error: '#E24056',
+        },
+      },
+    ],
   },
 };
