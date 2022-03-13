337b86b539f7c1c7a085b82dab5412015c22efee
KotaTanaka <te11k.tanaka@gmail.com>
2022-03-14 01:36:58 +0900

    [feat] markup login page

--- app/containers/Login.tsx
+++ app/containers/Login.tsx
@@ -6,8 +6,24 @@ const Login = () => {
   const { authorizeUrl } = useLoaderData<ILoginLoader>();
 
   return (
-    <div className="mt-16 text-center">
-      <a href={authorizeUrl}>ログイン</a>
+    <div className="flex justify-center mt-16">
+      <div className="shadow-xl w-104 daisy-card bg-neutral">
+        <div className="text-center daisy-card-body">
+          <p className="mb-6 leading-loose">
+            あなたが Spotify で最も聴いている
+            <br />
+            曲とアーティストのランキングを閲覧できます
+          </p>
+          <div className="justify-center daisy-card-actions">
+            <a
+              className="daisy-btn daisy-btn-wide daisy-btn-primary"
+              href={authorizeUrl}
+            >
+              Spotify アカウントでログイン
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
